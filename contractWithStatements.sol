6f82268697e8b3cb9ceecc7cb5f5e9a709b435d6
Navdeep <108007267+nxvtej@users.noreply.github.com>
2023-06-26 11:00:25 +0530

    Create contractWithStatements.sol

---
+++ contractWithStatements.sol
@@ -0,0 +1,30 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract ContractWithStatements {
+    address public owner;
+    uint public value;
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    function setValue(uint _value) external {
+        require(msg.sender == owner, "Only the owner can set the value.");
+        value = _value;
+    }
+
+    function increaseValue(uint _amount) external {
+        uint oldValue = value;
+        value += _amount;
+        assert(value >= oldValue);
+    }
+
+    function withdraw(uint amount) external {
+        require(amount <= value, "Insufficient balance.");
+        value -= amount;
+        if (value < 100) {
+            revert("Minimum balance requirement not met."); 
+        }
+    }
+}
