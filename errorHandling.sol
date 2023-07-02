930322f67368b00532ceb89a2ddcd166347aabff
Navdeep <108007267+nxvtej@users.noreply.github.com>
2023-07-02 18:26:11 +0530

    Update and rename contractWithMultipleStatements.sol to errorHandling.sol

--- errorHandling.sol
+++ errorHandling.sol
@@ -7,18 +7,17 @@ contract errHand
  function enterEvenviaRequire(uint256 _number) public pure returns(string memory) {
     require((_number%2==0),"number is not divisible by two  (error handled by require)");
     return("entered number is even");
-}
-
-
+    }
 
  function enterEvenviaRevert(uint256 _number) public pure returns(string memory) {
     if(_number%2 !=0 )
     revert("entered number is odd  (error handled by revert)");
     return("entered number is even");
-}
+    }
 
  function enterEvenviaAssert(uint256 _number) public pure returns(string memory){
 
         assert(_number > 0);
         return("return statement executed means assert condition is true");
-}}
+    }
+}
